1a3bca408652207c507579484265f9e9dc97d126
xinlei <wang.xinlei15@gmail.com>
2014-04-25 11:53:22 -0700

    Create a new rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Paddledesigner::Application.config.secret_key_base = 'c1bbe11e0197a678272499ca30c10d2e7daab9a7787f774f4bd7bfc360a4f02dccf4f680676886c7a5e2fd9493a58e9936313a9ba20d67d9431ffe99e77b33c7'
